75a61c8936c3e3cf879c3449340bb9cb8c59bcec
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2023-12-10 00:01:38 +0100

    Add ignore security check

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -8,7 +8,7 @@ from . import db
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
+        SECRET_KEY="dev",  # nosec
         DATABASE=os.path.join(app.instance_path, "ScoreOracle.sqlite"),
     )
 
