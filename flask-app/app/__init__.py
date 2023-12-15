a796a56b7bf0128112d981977083de815bfad251
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2023-12-15 23:27:15 +0100

    Add index page

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from app import db
-from app.views import auth
+from app.views import auth, blog
 from flask import Flask
 
 
@@ -31,5 +31,7 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
