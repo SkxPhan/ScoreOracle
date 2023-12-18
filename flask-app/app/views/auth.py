6ef96f466d87dceebe286287532034094f25f221
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2023-12-18 22:20:38 +0100

    Fix bug comma

--- flask-app/app/views/auth.py
+++ flask-app/app/views/auth.py
@@ -91,11 +91,8 @@ def load_logged_in_user():
     else:
         g.user = (
             get_db()
-            .execute(
-                "SELECT * FROM user WHERE id = ?",
-                (user_id,),
-            )
-            .fetchone(),
+            .execute("SELECT * FROM user WHERE id = ?", (user_id,))
+            .fetchone()
         )
 
 
