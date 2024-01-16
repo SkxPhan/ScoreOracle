0e3055feb2422e506211b0bc3774d3031a0b9cf1
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-01-16 10:15:23 +0900

    Fix test

--- flask-app/tests/unit/test_blog.py
+++ flask-app/tests/unit/test_blog.py
@@ -4,8 +4,7 @@ from app.db import get_db
 
 def test_index(client, auth):
     response = client.get("/")
-    assert b"Log In" in response.data
-    assert b"Register" in response.data
+    assert b"login" in response.data
 
     auth.login()
     response = client.get("/")
